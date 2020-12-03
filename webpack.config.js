3f45d0df389f085096d4f335aa652b4b9a6d08ea
jusheen <jusheen9@gmail.com>
2020-12-02 17:26:02 -0800

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+const SRC_DIR = path.join(__dirname, "/client/src");
+const DIST_DIR = path.join(__dirname, "/client/public");
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: "bundle.js",
+    path: DIST_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: [".js", ".json", ".jsx", ".css"],
+  },
+
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "/client/public"),
+  },
+};
