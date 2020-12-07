bc48cccb5f118c5ecc38cdc8135ce8ea1106e0a8
thehlau <thehlau@gmail.com>
2020-12-06 23:11:57 -0800

    add Save and Share svg icons

--- client/src/components/HotelCard.jsx
+++ client/src/components/HotelCard.jsx
@@ -77,6 +77,7 @@ const Rating = styled.div`
   right: 5%;
   top: 30%;
   border: 1px solid black;
+  border-radius: 3px;
   min-height: 50px;
   width: 20%;
   padding: 2px;
@@ -94,6 +95,7 @@ const PricePane = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
 `;
 
 const Price = styled.div`
@@ -107,6 +109,20 @@ const PriceInfo = styled.p`
   padding:0;
   margin: 0;
 `
+const Save = styled.div`
+  position: absolute;
+  height: 5%;
+  width: 5%;
+  right: 10%;
+  top: 10%;
+`;
+const Suggest = styled.div`
+  position: absolute;
+  height: 5%;
+  width: 5%;
+  right: 10%;
+  bottom: 10%;
+`;
 
 
 const ratingStrings = ['none', 'Poor', 'Fair', 'Good', 'Great', 'Excellent']
@@ -146,10 +162,16 @@ export default function HotelCard({ HotelData, numOfNights }) {
           null
         }
         {(HotelData.rating) ?
-        <Rating>{HotelData.rating}/5  {ratingStrings[HotelData.rating]}</Rating>:
-        null}
+          <Rating>{HotelData.rating}/5  {ratingStrings[HotelData.rating]}</Rating> :
+          null}
       </CenterSection>
       <PricePane>
+        <Save >
+          <svg aria-hidden="true" role="presentation" focusable="false" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" className="sc-fzqzlV sc-fzqLLg kCMTKY"><path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path></svg>
+        </Save>
+        <Suggest >
+          <svg aria-hidden="true" role="presentation" focusable="false" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" className="sc-fzqzlV gMJRj"><g vectorEffect="non-scaling-stroke" transform="translate(0,0)scale(1,1)" fill="none" fillRule="evenodd" stroke="#000" strokeWidth="2"><path d="m27 18v9c0 1.1045695-.8954305 2-2 2h-18c-1.1045695 0-2-.8954305-2-2v-9"></path><path d="m4.5 14.5h23z" transform="matrix(0 1 -1 0 30.5 -1.5)"></path><path d="m6 13 9.2928932-9.29289322c.3905243-.39052429 1.0236893-.39052429 1.4142136 0l9.2928932 9.29289322"></path></g></svg>
+        </Suggest>
         <Price>
           ${(HotelData.Price / numOfNights).toFixed(2)}
           <PriceInfo>Nightly price per room</PriceInfo>
